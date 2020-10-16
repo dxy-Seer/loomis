cfb23b9ca8dc232f2e364f29ccbe02763f50715a
dxy-Seer <semfuchs211@gmail.com>
2020-10-16 13:24:10 +0200

    Add template command

---
+++ commands/template.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    message.reply('This is a template command!')
+
+    // Take note, you can make a command by putting the outcome here
+}
+
+// Put the command args here (What calls it)
+module.exports.help = {
+    name: "temp",
+    aliases: []
+}
